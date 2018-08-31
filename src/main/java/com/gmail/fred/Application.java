a261f24d3b3af87022901d89559cd6410b06314c
fredjhosnick <fredjhosnickrios@gmail.com>
2018-08-31 17:19:18 +0300

    Hello

---
+++ src/main/java/com/gmail/fred/Application.java
@@ -0,0 +1,36 @@
+package com.gmail.fred;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(final UserService userService) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                userService.addUser("admin", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", UserRole.ADMIN, "", "", "");
+                userService.addUser("user", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", UserRole.USER, "", "","");
+            }
+        };
+    }
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+}
---
+++ src/main/java/com/gmail/fred/MyController.java
@@ -0,0 +1,94 @@
+package com.gmail.fred;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ShaPasswordEncoder passwordEncoder;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        User user = (User)SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String login = user.getUsername();
+        CustomUser dbUser = userService.findByLogin(login);
+
+        model.addAttribute("login", login);
+        model.addAttribute("roles", user.getAuthorities());
+        model.addAttribute("email", dbUser.getEmail());
+        model.addAttribute("phone", dbUser.getPhone());
+        model.addAttribute("comment", dbUser.getComment());
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@RequestParam(required = false) String email,
+                         @RequestParam(required = false) String phone,
+                         @RequestParam(required = false) String comment){
+        User user = (User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String login = user.getUsername();
+        userService.updateUser(login, email, phone,comment);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/newuser", method = RequestMethod.POST)
+    public String update(@RequestParam String login,
+                         @RequestParam String password,
+                         @RequestParam(required = false) String email,
+                         @RequestParam(required = false) String phone,
+                         @RequestParam(required = false) String comment,
+                         Model model) {
+        String passHash = passwordEncoder.encodePassword(password, null);
+
+        if ( ! userService.addUser(login, passHash, UserRole.USER, email, phone,comment)) {
+            model.addAttribute("exists", true);
+            model.addAttribute("login", login);
+            return "register";
+        }
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @RequestMapping("/unauthorized")
+    public String unauthorized(Model model){
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("login", user.getUsername());
+        return "unauthorized";
+    }
+}
